6024ceb64616c8452929e710d81088cb2ddc0800
rodrigojmayer <rodrigojmayer>
2023-02-22 09:15:15 +0100

    dbjson

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {   Grid, makeStyles, Typography } from '@material-ui/core';
 import {  Button, createTheme, ThemeProvider } from '@mui/material'
 import { blue, grey, red } from '@material-ui/core/colors';
@@ -30,7 +30,7 @@ const useStyles = makeStyles({
 
 export default function Game() {
   let gameOff = false;
-
+  let nextTurn;
   const [ turn, setTurn ] = useState("X")
   const [ buttonsState, setButtonsState ] = useState([ 
     {id: 0, free: 1, mark: ""},
@@ -57,7 +57,8 @@ export default function Game() {
       if(button.id === buttonsState.id && buttonsState.free){
         val = 0;
         selected = turn;
-        setTurn(turn==="X"?"O":"X")
+        nextTurn = turn==="X"?"O":"X";
+        setTurn(nextTurn)
       
       
       }
@@ -66,9 +67,18 @@ export default function Game() {
       })
 
       setButtonsState(updateButtons);
+      // console.log("its entering here??=?")
+      // console.log(updateButtons)
+      // update0(0);   
+      fetch('http://localhost:8000/turns/1', {
+        method: 'PATCH',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({"buttonsStates": updateButtons, "turn": nextTurn})
+      })
       
   }
-  console.log(buttonsState[0].mark)
+  // console.log(buttonsState[0].mark)
+  // Finish conditionals
   if((buttonsState[0].mark!=="" && buttonsState[0].mark === buttonsState[1].mark && buttonsState[0].mark === buttonsState[2].mark) || 
     (buttonsState[3].mark!=="" && buttonsState[3].mark === buttonsState[4].mark && buttonsState[3].mark === buttonsState[5].mark) ||
     (buttonsState[6].mark!=="" && buttonsState[6].mark === buttonsState[7].mark && buttonsState[6].mark === buttonsState[8].mark) ||
@@ -77,13 +87,13 @@ export default function Game() {
     (buttonsState[2].mark!=="" && buttonsState[2].mark === buttonsState[5].mark && buttonsState[2].mark === buttonsState[8].mark) ||
     (buttonsState[0].mark!=="" && buttonsState[0].mark === buttonsState[4].mark && buttonsState[0].mark === buttonsState[8].mark) ||
     (buttonsState[2].mark!=="" && buttonsState[2].mark === buttonsState[4].mark && buttonsState[2].mark === buttonsState[6].mark)){
-      alert(`Winner ${turn==="X"?"O":"X"}`)
       gameOff = true;
+      alert(`Winner ${turn==="X"?"O":"X"}`)
       
       // classes.grid
   }
   const restart = () => {
-    setButtonsState([ 
+    const restartButtons = [ 
       {id: 0, free: 1, mark: ""},
       {id: 1, free: 1, mark: ""},
       {id: 2, free: 1, mark: ""},
@@ -93,7 +103,15 @@ export default function Game() {
       {id: 6, free: 1, mark: ""},
       {id: 7, free: 1, mark: ""},
       {id: 8, free: 1, mark: ""},
-    ]);
+    ]
+    setButtonsState(restartButtons);
+    
+
+    fetch('http://localhost:8000/turns/1', {
+      method: 'PATCH',
+      headers: {"Content-type": "application/json"},
+      body: JSON.stringify({"buttonsStates": restartButtons, "turn": turn})
+    })
 
   }
 
@@ -104,12 +122,43 @@ export default function Game() {
       return blue[600]
   } 
 
+
+
+  const [number, setNumber] = useState(0)
+  useEffect(() => {
+    if(!gameOff){
+      const interval = setInterval(() => {
+        setNumber(prev=>prev+1);
+        // console.log("UseEffect running");
+        fetch('http://localhost:8000/turns/')
+        .then(res => res.json())
+        .then(data => {
+          console.log(data[0].buttonsStates)
+          console.log(gameOff)
+          setButtonsState(data[0].buttonsStates)
+          setTurn(data[0].turn)
+          // setButtonsState(data)
+        })
+
+      }, 1000);
+    return () => {
+      clearInterval(interval);
+    }
+
+  }
+  },[gameOff]);
+
+
+
   return (
 
     <ThemeProvider theme={theme}>
       <div className={classes.div}>
         <Typography variant='h6'>
           TicTacToe
+        </Typography>
+        <Typography variant='h6'>
+          It's turn of {turn} {number}
         </Typography>
 
         <Grid container  spacing={2} className={classes.grid} >
